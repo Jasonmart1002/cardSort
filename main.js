a0c4795d2d2d4289f40e6997c327ae6e1acd7a51
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-02-14 21:48:01 +0000

    k

--- main.js
+++ main.js
@@ -24,34 +24,8 @@ function dealCards(){
  
         document.querySelector('#cardDisplay').innerHTML +=  card;
     }
-    document.getElementById("disable").disabled = true;
+    document.getElementById("disable0").disabled = true;
 }
-
-
-// Dealcards
-function dealCards(){
-
-    // Get input value
-    let userSelection = +document.querySelector('#input').value
-    // Get the random cards
-    for ( i = 0; i < userSelection; i++){
-        //Card numbers
-        const values = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-        let cardValue = values[Math.floor(Math.random() * values.length)];
-        //Card suits
-        const suits = ['&#9824;', ' &#9827;', '&#9829;', '&#9830;']
-        let cardSuit = suits[Math.floor(Math.random() * suits.length)]
-        let card = `<div class="card">
-                    <div class="card-body">
-                        ${cardSuit} ${cardValue} ${cardSuit}
-                    </div>
-                    </div>`;
- 
-        document.querySelector('#cardDisplay').innerHTML +=  card;
-    }
-    document.getElementById("disable").disabled = true;
-}
-
 
 
 
