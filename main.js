962012198eeb1786f58bc82a6b6f49719ba2d5ce
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-02-14 15:46:39 -0500

    Revert "kev1"
    This reverts commit d8825b45bbc3dd984e4dedc87db690ad0b44b4ee.

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
-const values = [,2,3,4,5,6,7,8,9,10,11,12,13];
-let cardValue = values[Math.floor(Math.random() * values.length)];
-const suits = ['&#9824;', '	&#9827;', '&#9829;', '&#9830;']
-let cardSuit = suits[Math.floor(Math.random() * suits.length)]
+// Random cards
 
-  let card = `<div class="card">
-                  <div class="card-body">
-                    ${cardValue} ${cardSuit} ${cardValue}
-                  </div>
-                </div>`;
+function dealCards(){
 
- function dealCards(){
-   
-  let userSelection = +document.querySelector('#input').value
-  
-  for ( i = 0; i < userSelection; i++){
-    document.querySelector('#cardDisplay').innerHTML +=  card;
-  }
- }
+    // Get input value
+    let userSelection = +document.querySelector('#input').value
+
+    // Get the random cards
+    for ( i = 0; i < userSelection; i++){
+
+        //Card numbers
+        const values = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+        let cardValue = values[Math.floor(Math.random() * values.length)];
+
+        //Card suits
+        const suits = ['&#9824;', '	&#9827;', '&#9829;', '&#9830;']
+        let cardSuit = suits[Math.floor(Math.random() * suits.length)]
+
+        let card = `<div class="card">
+                    <div class="card-body">
+                        ${cardValue} ${cardSuit} ${cardValue}
+                    </div>
+                    </div>`;
  
- 
- function sortCards(){
-  
-    if (isProcessing) { return; }
-    
-  const bubbleSort = (arr) => {
-  let wall = arr.length - 1; 
-  while (wall > 0){
-      let index = 0;
-      while (index < wall) {
-        if (arr[index] > arr[index + 1]) { 
-          let aux = arr[index]; 
-          arr[index] = arr[index + 1];
-          arr[index + 1] = aux;
-        }
-        console.log(xxxxxxx)
-        index++;
-      }
-      wall--; 
-  }
-return arr;
-};
-  document.querySelector('#cardSorter').innerHTML += card2
-
+        document.querySelector('#cardDisplay').innerHTML +=  card;
+    }
 }
+
+
+
+const bubbleSort = (arr) => {
+    let wall = arr.length - 1; //we start the wall at the end of the array
+    while (wall > 0){
+        let index = 0;
+        while (index < wall) {
+          //compare the adjacent positions, if the right one is bigger, we have to swap
+          if (arr[index] > arr[index + 1]) { 
+            let aux = arr[index]; 
+            arr[index] = arr[index + 1];
+            arr[index + 1] = aux;
+          }
+          index++;
+        }
+        wall--; //decrease the wall for optimization
+    }
+	return arr;
+};
