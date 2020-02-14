54b3d471cd602bad7f31fb48e8e369557d6238ff
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-02-14 15:50:08 -0500

    fix

--- main.js
+++ main.js
@@ -24,12 +24,37 @@ function dealCards(){
  
         document.querySelector('#cardDisplay').innerHTML +=  card;
     }
+    document.getElementById("disable").disabled = true;
 }
 
 
 
+function dealCards(){
+    // Get input value
+    let userSelection = +document.querySelector('#input').value
+    // Get the random cards
+    for ( i = 0; i < userSelection; i++){
+        //Card numbers
+        const values = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+        let cardValue = values[Math.floor(Math.random() * values.length)];
+        //Card suits
+        const suits = ['&#9824;', ' &#9827;', '&#9829;', '&#9830;']
+        let cardSuit = suits[Math.floor(Math.random() * suits.length)]
+        let card = `<div class="card">
+                    <div class="card-body">
+                        ${cardSuit} ${cardValue} ${cardSuit}
+                    </div>
+                    </div>`;
+ 
+        document.querySelector('#cardDisplay').innerHTML +=  card;
+    }
+    document.getElementById("disable").disabled = true;
+}
+
+//Bubblesort
 const bubbleSort = (arr) => {
-    let wall = arr.length - 1; //we start the wall at the end of the array
+    //we start the wall at the end of the array
+    let wall = arr.length - 1; 
     while (wall > 0){
         let index = 0;
         while (index < wall) {
@@ -41,7 +66,8 @@ const bubbleSort = (arr) => {
           }
           index++;
         }
-        wall--; //decrease the wall for optimization
+        //decrease the wall for optimization
+        wall--; 
     }
-	return arr;
+    return arr;
 };
